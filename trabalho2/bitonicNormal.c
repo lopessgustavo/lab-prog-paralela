b5a4ffb41d9c7172b5757f618773e4e389984db2
holandafelipeuff <felipeholanda@id.uff.br>
2019-12-03 02:32:16 -0300

    mudanca do codigo do bitonic sort

---
+++ trabalho2/bitonicNormal.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+// Ordena uma dada sequência bitônica
+void BitonicSortCut(int *vector, int n, int cresc){
+    if (n > 1) {
+        int m = n/2;
+        for(int i = 0; i < m; i++)
+            if ((cresc == 1 && vector[i] > vector[i+m]) || 
+                (cresc == 0 && vector[i] < vector[i+m])){
+                int aux = vector[i];
+                vector[i] = vector[i+m];
+                vector[i+m] = aux; 
+            }
+        BitonicSortCut(vector, m, cresc);
+        BitonicSortCut(vector + m, m, cresc);
+    }
+}
+// Transforma a sequência em sequência bitônica
+// e a ordena
+void BitonicSort(int *vector, int n, int cresc){
+    if (n > 1) {
+        int m = n/2;
+        BitonicSort(vector, m, 1); 
+        BitonicSort(vector + m, m, 0);
+        BitonicSortCut(vector, n, cresc);
+    }
+}
+ 
+int main (){
+ 
+    int vector[ ] = {5, 1, 6, 9, 8, 7, 0, 3};
+    int n = 8;
+ 
+    BitonicSort(vector, n, 1);
+ 
+    for(int i = 0; i < n; i++)
+        printf("%d ", vector[i]);
+}
